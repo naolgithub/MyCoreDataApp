78f14ab1cd7204ccb0f3f2284644829dd11afdc8
naolgithub <naolcovered@gmail.com>
2024-12-26 19:09:33 +0300

    Initial Commit

---
+++ MyCoreDataApp/MyCoreDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataAppApp.swift
+//  MyCoreDataApp
+//
+//  Created by Naol Abera on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCoreDataApp/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCoreDataApp
+//
+//  Created by Naol Abera on 26/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
